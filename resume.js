80b4180c6b6b216fe037936ce10b515f71079305
Loulyb <louisejanum@hotmail.com>
2024-02-14 15:36:44 +0100

    tilføjning af book bord

--- resume.js
+++ resume.js
@@ -1,3 +1,5 @@
+//start på slideshow
+
 document.addEventListener("DOMContentLoaded", function() {
     const slides = document.querySelectorAll('.slidess');
 
@@ -49,9 +51,49 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+//Slut på slide show
 
+//Start på book bord
 
+document.getElementById('openBooking').addEventListener('click', function() {
+    let bookingPopup = document.getElementById('bookingPopup');
+    if (bookingPopup.style.display === 'block') {
+        bookingPopup.style.display = 'none'; // Skjul popup'en, hvis den allerede er åben
+    } else {
+        bookingPopup.style.display = 'block'; // Ellers åbn popup'en
+    }
+});
 
+document.getElementById('closeBooking').addEventListener('click', function() {
+    let bookingPopup = document.getElementById('bookingPopup');
+    bookingPopup.style.display = 'none'; // Luk popup'en
+});
+
+// Opret et tomt array til at gemme bookingoplysninger
+let bookings = [];
+
+document.getElementById('bookingForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let formData = new FormData(this);
+    let bookingDetails = {};
+    formData.forEach(function(value, key){
+        bookingDetails[key] = value;
+    });
+
+    // Tilføj bookingoplysningerne til bookings-arrayet
+    bookings.push(bookingDetails);
+
+    // Udskriv bookingoplysningerne til konsollen
+    console.log(bookingDetails); 
+
+    // Udskriv alle bookinger til konsollen
+    console.log(bookings);
+
+    // Luk booking-popup'en
+    document.getElementById('bookingPopup').style.display = 'none';
+});
+
+//Slut på book bord
 
 
 
